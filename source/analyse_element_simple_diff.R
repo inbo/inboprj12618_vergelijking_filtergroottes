763c473f7d9add1a48faa2bd2b32a3dd917db258
VERSCHELDE <pieter.verschelde@inbo.be>
2025-04-17 16:57:53 +0200

    Verbeteringen aan het finale rapportdocument (nog in progress)

--- source/analyse_element_simple_diff.R
+++ source/analyse_element_simple_diff.R
@@ -7,13 +7,19 @@ analyse_element_simple_diff <-  function(data, chem_element = NULL) {
   formula <- NULL
 
   data_el_pivot <- data |> dplyr::filter(element == chem_element) |> pull(wide)
-  data_el_pivot <- data_el_pivot[[1]]
+
+  diff_name <- paste0("diff_log10(", chem_element, ")")
+  mean_name <- paste0("mean_log10(", chem_element, ")")
+  data_el_pivot <- data_el_pivot[[1]] |>
+    mutate(element = chem_element,
+           !!diff_name := diff_log10_value,
+           !!mean_name := mean_log10_value)
 
 
   minrange <- min(data_el_pivot$mean_log10_value, na.rm = TRUE)
   maxrange <- max(data_el_pivot$mean_log10_value, na.rm = TRUE)
 
-  moddiff_formula <- diff_log10_value ~ mean_log10_value
+  moddiff_formula <- as.formula(paste0("`", diff_name, "` ~ `", mean_name, "`"))
 
   if(!all(is.finite(minrange)) || !all(is.finite(maxrange)) || minrange == maxrange) {
     return(list(formula = moddiff_formula,
@@ -24,11 +30,14 @@ analyse_element_simple_diff <-  function(data, chem_element = NULL) {
                 max = NA))
   }
 
-  newdata_diff <-
-    expand.grid(mean_log10_value = seq(minrange, maxrange, length = 20))
+
+  # For expand.grid, we need to use a different approach with rlang
+  newdata_params <- list(seq(minrange, maxrange, length = 20))
+  names(newdata_params) <- mean_name  # Assign the name directly
+  newdata_diff <- do.call(expand.grid, newdata_params)
 
   moddiff <- try(lm(moddiff_formula, data = data_el_pivot))
-  if (class(moddiff) == "try-error") {
+  if (inherits(moddiff, "try-error")) {
     return(list(moddiff = "Error in model"))
   }
   moddiff_summary <- summary(moddiff)
@@ -40,17 +49,24 @@ analyse_element_simple_diff <-  function(data, chem_element = NULL) {
     geom_abline(intercept = 0, slope = 0, linetype = "dashed") +
     geom_smooth(method = "loess",formula = y ~ x) +
     geom_point() +
-    labs(title = "Pearson residuals plot", x = "fitted", y = "residuals")
+    labs(title = "Pearson residuals plot", x = "fitted", y = "pearson residuals")
 
   p_res_hist_diff <- ggplot(data = data.frame(x = resid(moddiff, type = "pe")),
-                            aes(x = x)) + geom_histogram(bins = 20)
+                            aes(x = x)) + geom_histogram(bins = 20) +
+    labs(x = "Pearson Residuals", y = "Count")
 
   p_res_qq_diff <- ggplot(data = data.frame(x = resid(moddiff, type = "pe")),
                           aes(sample = x)) +
-    geom_qq() + geom_qq_line()
+    geom_qq() + geom_qq_line() +
+    labs(x = "theoretical quantiles", y = "sample quantiles")
 
   plot_resid_diff <- (p_fit_res_diff | (p_res_hist_diff / p_res_qq_diff))
-  #
+
+
+  # residual criteria
+  data_conclusions <- nobs_discussion(data_el_pivot)
+  norm_conclusions <- normality_discussion(resid(moddiff, type = "pe"))
+
   # # PREDICTION PLOTS
   preddiff <- bind_cols(
     newdata_diff,
@@ -78,104 +94,20 @@ analyse_element_simple_diff <-  function(data, chem_element = NULL) {
                            "Slope:", round(slope, 3), "(p =", round(p_slope, 3), ")"),
              hjust = 0, vjust = 1)
 
+  ## Prediction conclusions
+  # Check if the model is significant
+  model_conclusions <- trend_diff_discussion(moddiff_summary)
+
 
   list(data = data_el_pivot,
        formula = moddiff_formula,
-       model_summary = moddiff_summary,
+       model = moddiff_summary,
        plot_resid = plot_resid_diff,
-       plot_fit = predplot_diff,
+       plot_pred = predplot_diff,
+       model_conclusions = list(data = data_conclusions,
+                                res = norm_conclusions,
+                                model = model_conclusions),
        min =  minrange,
        max= maxrange)
 }
 
-################################################################################
-
-analyse_element_simple_fmod <- function(data, chem_element) {
-  require("tidyverse")
-  require("patchwork")
-
-  data_el <- data |> filter(element == chem_element) |> pull(wide)
-
-  newdata_fmod <-
-    expand.grid(
-      log10_value =
-        seq(min(data_el$log10_value, na.rm = TRUE),
-            max(data_el$log10_value, na.rm = TRUE),
-            length = 20),
-      filter = na.omit(unique(data_el$filter)),
-      matrix = unique(data_el$matrix),
-      lab_id = 0)
-
-  #fmod: value ~ filter
-
-  mod_formula <- log10_value ~ filter + (1|lab_id)
-  mod <- lme4::lmer(mod_formula, data = data_el)
-  mod_summary <- summary(mod)
-
-  ### Residual plots fmod
-
-  p_fit_res <-
-    ggplot(data.frame(resid = resid(mod, type = "pe"), fit = fitted(mod)),
-         aes(x = fit, y = resid)) +
-    geom_abline(intercept = 0, slope = 0, linetype = "dashed") +
-    geom_smooth(method = "loess",formula = y ~ x) +
-    geom_point() +
-    labs(title = "Pearson residuals plot")
-
-  p_res_hist <- ggplot(data = data.frame(x = resid(mod, type = "pe")),
-                       aes(x = x)) + geom_histogram(bins = 30)
-
-  p_res_qq <- ggplot(data = data.frame(x = resid(mod, type = "pe")),
-                     aes(sample = x)) +
-    geom_qq() + geom_qq_line()
-
-  plot_resid <- (p_fit_res | (p_res_hist / p_res_qq))
-
-  ### Prediction plot fmod
-
-  plotdata_fmod <-
-    bind_cols(
-      newdata_fmod,
-      predictInterval(mod,
-                      newdata = newdata_fmod,
-                      level = 0.95,
-                      which = "fixed",
-                      include.resid.var = FALSE,
-                      fix.intercept.variance = TRUE))
-  plotdata_fmod_pivot <- plotdata_fmod |>
-    pivot_wider(names_from = filter, values_from = c(fit, lwr, upr))
-
-
-  plot_model <-
-    ggplot() +
-    geom_jitter(data = data_el, aes(x = filter, y = log10_value, color = filter), width = 0.05) +
-    geom_point(plotdata_fmod, mapping = aes(y = fit, x = filter), color = "black") +
-    geom_errorbar(plotdata_fmod, mapping = aes(x = filter, ymin = lwr, ymax = upr)) +
-    labs(title = "Prediction plot", xlab = "filter", ylab = paste0("log10(", chem_element, ")"))
-
-  plot_model_fit_only <-
-    ggplot(plotdata_fmod, aes(x = filter, y = fit, ymin = lwr, ymax = upr, color = filter)) +
-    geom_point() +
-    geom_errorbar() +
-    labs(x = "filter", y = paste0("log10(", chem_element, ")"))
-
-  plot_model_ref <-
-    ggplot(data.frame(est = coef(mod_summary)[2,1],
-                      lwr = coef(mod_summary)[2,1] + qnorm(0.025) * coef(mod_summary)[2,2],
-                      upr = coef(mod_summary)[2,1] + qnorm(0.975) * coef(mod_summary)[2,2]),
-           mapping = aes(x = 1, y = est, ymin = lwr, ymax = upr)) +
-    geom_errorbar(width = 0.5) +
-    geom_point() +
-    geom_hline(yintercept = 0, linetype = "dashed", color = "blue", linewidth = 2) +
-    labs(y = "log10(45µm) - log10(20µm)", x = chem_element) +
-    theme(axis.ticks.x = element_blank(), axis.text.x = element_blank())
-
-  plot_pred <- plot_model | (plot_model_ref / plot_model_fit_only)
-
-  list(data = data_el,
-       formula = mod_formula,
-       model = mod_summary,
-       plot_resid = plot_resid,
-       plot_pred = plot_pred)
-
-}
--- source/analyse_element_simple_fmod.R
+++ source/analyse_element_simple_fmod.R
@@ -3,13 +3,22 @@ analyse_element_simple_fmod <- function(data, chem_element) {
   require("patchwork")
 
   data_el <- data |> filter(element == chem_element) |> pull(long)
+
+  log_name <- paste0("log10(", chem_element, ")")
   data_el <- data_el[[1]] |>
     filter(is.finite(log10_value),
            !is.na(filter),
            !is.na(value),
-           !all_below_loq)
+           !all_below_loq) |>
+    mutate(element = chem_element,
+           !!log_name := log10_value,
+           filter2 = factor(filter,
+                            levels = sort(unique(filter)),
+                            labels = c("20µm", "45µm")))
 
-  mod_formula <- log10_value ~ filter + (1|lab_id)
+  #mod_formula <- log10_value ~ filter + (1|lab_id)
+  mod_formula <- as.formula(paste0("`", log_name, "` ~ filter + (1|lab_id)"))
+
 
   #check if multiple lab_ids
   if (length(unique(data_el$lab_id)) < 3) {
@@ -23,19 +32,31 @@ analyse_element_simple_fmod <- function(data, chem_element) {
     )
   }
 
-  newdata_fmod <-
-    expand.grid(
-      log10_value =
-        seq(from = min(data_el$log10_value, na.rm = TRUE),
-            to = max(data_el$log10_value, na.rm = TRUE),
-            length = 20),
-      filter = na.omit(unique(data_el$filter)),
-      matrix = unique(data_el$matrix),
-      lab_id = 0)
+  minrange <- min(data_el$log10_value, na.rm = TRUE)
+  maxrange <- max(data_el$log10_value, na.rm = TRUE)
+
+  # newdata_fmod <-
+  #   expand.grid(
+  #     log10_value =
+  #       seq(from = minrange,
+  #           to = maxrange,
+  #           length = 20),
+  #     filter = na.omit(unique(data_el$filter)),
+  #     matrix = unique(data_el$matrix),
+  #     lab_id = 0)
+
+  # Create a named list for expand.grid
+  grid_params <- list()
+  grid_params[[log_name]] <- seq(from = minrange, to = maxrange, length = 20)
+  grid_params[["filter"]] <- na.omit(unique(data_el$filter))
+  grid_params[["matrix"]] <- unique(data_el$matrix)
+  grid_params[["lab_id"]] <- 0
+
+  # Use do.call to pass the list to expand.grid
+  newdata_fmod <- do.call(expand.grid, grid_params)
 
   #fmod: value ~ filter
 
-  mod_formula <- log10_value ~ filter + (1|lab_id)
   mod <- lme4::lmer(mod_formula, data = data_el)
   mod_summary <- summary(mod)
 
@@ -47,16 +68,27 @@ analyse_element_simple_fmod <- function(data, chem_element) {
     geom_abline(intercept = 0, slope = 0, linetype = "dashed") +
     geom_smooth(method = "loess",formula = y ~ x) +
     geom_point() +
-    labs(title = "Pearson residuals plot")
+    labs(x = "fitted values", y = "pearson residuals")
 
-  p_res_hist <- ggplot(data = data.frame(x = resid(mod, type = "pe")),
-                       aes(x = x)) + geom_histogram(bins = 30)
+  p_res_hist <-
+    ggplot(data = data.frame(x = resid(mod, type = "pe")),
+           aes(x = x)) +
+    geom_histogram(bins = 20) +
+    labs(x = "pearson residuals", y = "count")
 
-  p_res_qq <- ggplot(data = data.frame(x = resid(mod, type = "pe")),
-                     aes(sample = x)) +
-    geom_qq() + geom_qq_line()
+  p_res_qq <-
+    ggplot(data = data.frame(x = resid(mod, type = "pe")),
+           aes(sample = x)) +
+    geom_qq() +
+    geom_qq_line() +
+    labs(x = "theoretical quantiles", y = "sample quantiles")
 
   plot_resid <- (p_fit_res | (p_res_hist / p_res_qq))
+
+  ###
+  # residual criteria
+  data_conclusions <- nobs_discussion(data_el, factor = 2)
+  norm_conclusions <- normality_discussion(resid(mod, type = "pe"))
 
   ### Prediction plot fmod
 
@@ -68,17 +100,22 @@ analyse_element_simple_fmod <- function(data, chem_element) {
                       level = 0.95,
                       which = "fixed",
                       include.resid.var = FALSE,
-                      fix.intercept.variance = TRUE))
+                      fix.intercept.variance = TRUE)) |>
+    mutate(filter = factor(filter,
+                           levels = sort(unique(data_el$filter)),
+                           labels = c("20µm", "45µm")))
+
   plotdata_fmod_pivot <- plotdata_fmod |>
     pivot_wider(names_from = filter, values_from = c(fit, lwr, upr))
 
-
   plot_model <-
     ggplot() +
-    geom_jitter(data = data_el, aes(x = filter, y = log10_value, color = filter), width = 0.05) +
+    geom_jitter(data = data_el, aes(x = filter2, y = log10_value, color = filter2), width = 0.05) +
+    geom_line(data = data_el, aes(x = filter2, y = log10_value, group = lab_id),
+              color = "black", alpha = 0.2) +
     geom_point(plotdata_fmod, mapping = aes(y = fit, x = filter), color = "black") +
     geom_errorbar(plotdata_fmod, mapping = aes(x = filter, ymin = lwr, ymax = upr)) +
-    labs(title = "Prediction plot", xlab = "filter", ylab = paste0("log10(", chem_element, ")"))
+    labs(x = "filter", y = paste0("log10(", chem_element, ")"), color = "filter")
 
   plot_model_fit_only <-
     ggplot(plotdata_fmod, aes(x = filter, y = fit, ymin = lwr, ymax = upr, color = filter)) +
@@ -91,18 +128,25 @@ analyse_element_simple_fmod <- function(data, chem_element) {
                       lwr = coef(mod_summary)[2,1] + qnorm(0.025) * coef(mod_summary)[2,2],
                       upr = coef(mod_summary)[2,1] + qnorm(0.975) * coef(mod_summary)[2,2]),
            mapping = aes(x = 1, y = est, ymin = lwr, ymax = upr)) +
-    geom_errorbar(width = 0.5) +
+    geom_errorbar(width = 0.75) +
     geom_point() +
-    geom_hline(yintercept = 0, linetype = "dashed", color = "blue", linewidth = 2) +
-    labs(y = "log10(45µm) - log10(20µm)", x = chem_element) +
+    geom_hline(yintercept = 0, linetype = "dashed", color = "blue", linewidth = 1.3) +
+    xlim(0.5,1.5) +
+    labs(y = "log10(45µm) - log10(20µm)", x = paste("element:",chem_element)) +
     theme(axis.ticks.x = element_blank(), axis.text.x = element_blank())
 
   plot_pred <- plot_model | (plot_model_ref / plot_model_fit_only)
+
+  ### prediction conclusion
+  model_conclusions <- trend_fmod_discussion(mod_summary)
 
   list(data = data_el,
        formula = mod_formula,
        model = mod_summary,
        plot_resid = plot_resid,
-       plot_pred = plot_pred)
+       plot_pred = plot_pred,
+       model_conclusions = list(data = data_conclusions,
+                                res = norm_conclusions,
+                                model = model_conclusions))
 
 }
